ef6e7be25d33f7b512fe9d55bef39d207c8869ba
Pham Duc Tai <phamductai102703@gmail.com>
2024-08-19 14:42:16 +0700

    add coding challenge #3

---
+++ lab1/JavaScript Fundamentals – Part 1/coding-challenge-#3/index.js
@@ -0,0 +1,67 @@
+const calculateAverageScore = (team) => team.score.reduce((acc, current) => acc + current, 0) / 3
+
+let determineWinner = (team1, team2) => {
+  if (calculateAverageScore(team1) > calculateAverageScore(team2)) {
+    return team1.name
+  } else if (calculateAverageScore(team2) > calculateAverageScore(team1)) {
+    return team2.name
+  } else {
+    return "draw"
+  }
+} 
+
+let dophins = {
+  name: "Dophins",
+  score: [96, 108, 89]
+}
+
+let koalas = {
+  name: "Koalas",
+  score: [88, 91, 110]
+}
+
+console.log(determineWinner(dophins, koalas))
+
+determineWinner = (team1, team2) => {
+  if ((calculateAverageScore(team1) > calculateAverageScore(team2)) && (calculateAverageScore(team1) >= 100)) {
+    return team1.name
+  } else if (calculateAverageScore(team2) > calculateAverageScore(team1) && (calculateAverageScore(team2) >= 100)) {
+    return team2.name
+  } else {
+    return "draw"
+  }
+} 
+
+dophins = {
+  name: "Dophins",
+  score: [97, 112, 101]
+}
+
+koalas = {
+  name: "Koalas",
+  score: [109, 95, 123]
+}
+
+console.log(determineWinner(dophins, koalas))
+
+determineWinner = (team1, team2) => {
+  if ((calculateAverageScore(team1) > calculateAverageScore(team2)) && (calculateAverageScore(team1) >= 100)) {
+    return team1.name
+  } else if (calculateAverageScore(team2) > calculateAverageScore(team1) && (calculateAverageScore(team2) >= 100)) {
+    return team2.name
+  } else {
+    return "draw"
+  }
+} 
+
+dophins = {
+  name: "Dophins",
+  score: [97, 112, 101]
+}
+
+koalas = {
+  name: "Koalas",
+  score: [109, 95, 123]
+}
+
+console.log(determineWinner(dophins, koalas))

3b96960a4b126779d400de13c1cc18dfaf67b9cc
Pham Duc Tai <phamductai102703@gmail.com>
2024-08-26 14:34:16 +0700

    lab2

---
+++ lab2/Data Structures, Modern Operators and Strings/coding-challenge-#2/index.js
@@ -0,0 +1,51 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+}
+
+game.scored.forEach((score, index) => {
+  console.log(`Goal ${index + 1}: ${score}`)
+})
+
+const oddAvg = Object.values(game.odds).reduce((accumulator, currentValue) => accumulator += currentValue, 0) / Object.values(game.odds).length
+
+console.log(oddAvg.toFixed(2))
+
+
